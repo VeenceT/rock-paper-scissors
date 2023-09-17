2479c55f516bf1ecd5ce39bab8883ad7bb0a5bae
VeenceT <vincent.k.topla@gmail.com>
2023-09-17 21:17:44 +0200

    Create a function to play a 5 rounds game

--- script.js
+++ script.js
@@ -10,54 +10,58 @@
 // - Make the computer randomly choose rock, paper or scissors (getComputerChoice())
 // ---> return the result
 
-// ----- COMPUTER SELECTION -----
+let game = () => {
+  for (let i = 1; i <= 5; i++) {
+    // ----- COMPUTER SELECTION -----
+    let getComputerSelection = () => {
+      let getComputerChoice = Math.floor(Math.random() * 3 + 1);
 
-let getComputerSelection = () => {
-  let getComputerChoice = Math.floor(Math.random() * 3 + 1);
+      if (getComputerChoice === 1) {
+        let computerSelection = "rock";
+        return computerSelection;
+      } else if (getComputerChoice === 2) {
+        let computerSelection = "paper";
+        return computerSelection;
+      } else {
+        let computerSelection = "scissors";
+        return computerSelection;
+      }
+    };
+    // console.log(getComputerSelection());
 
-  if (getComputerChoice === 1) {
-    let computerSelection = "rock";
-    return computerSelection;
-  } else if (getComputerChoice === 2) {
-    let computerSelection = "paper";
-    return computerSelection;
-  } else {
-    let computerSelection = "scissors";
-    return computerSelection;
+    // ----- PLAYER SELECTION -----
+    let getPlayerSelection = () => {
+      let getPlayerInput = prompt("Choose between Rock, Paper & Scissors");
+      let playerSelection = getPlayerInput.toLowerCase();
+      return playerSelection;
+    };
+    // console.log(getPlayerSelection());
+
+    // ----- SINGLE ROUND -----
+    let playRound = (playerSelection, computerSelection) => {
+      console.log(`----- Game ${i}/5 -----`);
+      playerSelection = getPlayerSelection();
+      computerSelection = getComputerSelection();
+
+      console.log(`player: ${playerSelection}, computer: ${computerSelection}`);
+
+      if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+      ) {
+        return `🟢 You win, ${playerSelection} beats ${computerSelection}!`;
+      } else if (
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "scissors" && playerSelection === "paper")
+      ) {
+        return `🔴 You loose, ${computerSelection} beats ${playerSelection}!`;
+      } else {
+        return `🟠 Tie, you both played ${playerSelection}`;
+      }
+    };
+    console.log(playRound());
   }
 };
-// console.log(getComputerSelection());
-
-// ----- PLAYER SELECTION -----
-let getPlayerSelection = () => {
-  let getPlayerInput = prompt("Choose between Rock, Paper & Scissors");
-  let playerSelection = getPlayerInput.toLowerCase();
-  return playerSelection;
-};
-// console.log(getPlayerSelection());
-
-// ----- SINGLE ROUND -----
-let playRound = (playerSelection, computerSelection) => {
-  playerSelection = getPlayerSelection();
-  computerSelection = getComputerSelection();
-
-  console.log(`player: ${playerSelection}, computer: ${computerSelection}`);
-
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return ` 🟢 You win, ${playerSelection} beats ${computerSelection}!`;
-  } else if (
-    (computerSelection === "rock" && playerSelection === "scissors") ||
-    (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "scissors" && playerSelection === "paper")
-  ) {
-    return ` 🔴 You loose, ${computerSelection} beats ${playerSelection}!`;
-  } else {
-    return `🟠 Tie, you both played ${playerSelection}`;
-  }
-};
-
-console.log(playRound());
+game();
